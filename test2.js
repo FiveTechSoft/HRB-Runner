ec1a9c6a409a8c9a2f6464f02285c48835b90643
FiveTech Software <antonio.fivetech@gmail.com>
2020-02-01 19:47:11 +0100

    Update test2.js

--- test2.js
+++ test2.js
@@ -21,7 +21,7 @@ function QQOut() {
 }
 
 var hrb = new HRB();
-    hrb.download("https://www.modharbour.org/modharbour_samples/data/runner.hrb_").then((v)=>{
+    hrb.download("https://www.modharbour.org/modharbour_samples/data/runner.hrb_?1234").then((v)=>{
     // hrb.download( "test2.hrb" ).then((v)=>{
     console.log(v);
     hrb.apply();
